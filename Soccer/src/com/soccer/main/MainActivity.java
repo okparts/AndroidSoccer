10d605aecf0d46b6c52b150779d31476407dc7a8
Matt Strapko <okparts@gmail.com>
2013-12-29 23:24:21 -0500

    Create new Splash activity for DB inits.

--- Soccer/src/com/soccer/main/MainActivity.java
+++ Soccer/src/com/soccer/main/MainActivity.java
@@ -1,30 +1,22 @@
 package com.soccer.main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.soccer.constants.GameVars;
-import com.soccer.database.DBHelper;
 
 public class MainActivity extends Activity {
 
 	private Vibrator vibe;
 	private View newBtn, contBtn;
 	private boolean gameExists = true;
-	private DBThread fnameThread, lnameThread, cityThread, teamThread, nicknameThread = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,26 +37,7 @@ public class MainActivity extends Activity {
 		if (!gameExists) {
 			contBtn.setVisibility(View.INVISIBLE);
 	    }
-		
-		// NEW THREAD - build first names database table
-		fnameThread = new DBThread(this, GameVars.RES_FIRSTNAMES, GameVars.TABLE_FIRST_NAMES, GameVars.COLUMN_FIRST_NAME);
-		fnameThread.start();
-		
-		// NEW THREAD - build first names database table
-		lnameThread = new DBThread(this, GameVars.RES_LASTNAMES, GameVars.TABLE_LAST_NAMES, GameVars.COLUMN_LAST_NAME);
-		lnameThread.start();
-		
-		// NEW THREAD - build city names database table
-		cityThread = new DBThread(this, GameVars.RES_CITYNAMES, GameVars.TABLE_CITY_NAMES, GameVars.COLUMN_CITY_NAME);
-		cityThread.start();
-		
-		// NEW THREAD - build city names database table
-		teamThread = new DBThread(this, GameVars.RES_TEAMNAMES, GameVars.TABLE_TEAM_NAMES, GameVars.COLUMN_TEAM_NAME);
-		teamThread.start();
-		
-		// NEW THREAD - build city names database table
-		nicknameThread = new DBThread(this, GameVars.RES_NICKNAMES, GameVars.TABLE_NICKNAMES, GameVars.COLUMN_NICKNAME);
-		nicknameThread.start();
+
 	}
 
 	@Override
@@ -103,82 +76,5 @@ public class MainActivity extends Activity {
 		
 		// pause activity
 		super.onPause();
-	}
-
-	// new thread to add first names to the first name table
-	private static class DBThread extends Thread {
-		private boolean isRunning = false;
-		private String table, column;
-		private int resource;
-		private Context c;
-		
-		public DBThread(Context context, int resource, String table, String column) {
-			this.c = context;
-			this.resource = resource;
-			this.table = table;
-			this.column = column;
-		}
-		
-		@Override
-		public void run() {
-			isRunning = true;
-			while (isRunning) {
-				// try first names db
-				// variables
-				InputStream input = null;
-				BufferedReader reader = null;
-				DBHelper db = new DBHelper(c);
-				
-				// check/populate first names table
-				if (db.tableIsEmpty(table)) {
-					Log.d("DB CHECK!!", "First Name table is not populated!");
-					try {
-						input = c.getResources().openRawResource(resource);
-						reader = new BufferedReader(new InputStreamReader(input));
-						Log.d("File Success!!!", "Reading file for " + table);
-						String fn;
-						while ((fn = reader.readLine()) != null) {
-							String[] names = fn.split(",");
-							for (int i = 0; i < names.length; i++) {
-								// write first names to the database
-								db.addName(table, column, names[i]);
-							}
-						}
-					} catch(Exception e) {
-						Log.d("Failure!!", "Could not read file for " + table);
-					} finally {
-						if (input != null) {
-							try {
-								input.close();
-								Log.d("Success!!", "Input stream connection closed.");
-							} catch (IOException e) {
-								Log.d("Failure!!", "Problem closing input stream connection.");
-							}
-						}
-						if (reader != null) {
-							try {
-								reader.close();
-								Log.d("Success!!", "Buffered reader closed.");
-							} catch (IOException e) {
-								Log.d("Failure!!", "Problem closing buffered reader.");
-							}
-						}
-						db.closeDB();
-						Log.d("Success!!", "Database closed.");
-					}
-				} else {
-					db.closeDB();
-					Log.d("Success!!", "Database closed.");
-					Log.d("DB CHECK!!", "First Name table is already populated!");
-				}
-				
-				// end the first names thread
-				this.close();
-			}
-		}
-		
-		public void close() {
-			isRunning = false;
-		}
 	}
 }
--- Soccer/src/com/soccer/main/Splash.java
+++ Soccer/src/com/soccer/main/Splash.java
@@ -1,12 +1,28 @@
 package com.soccer.main;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.soccer.constants.GameVars;
+import com.soccer.database.DBHelper;
 
 public class Splash extends Activity {
+	
+	InputStream input = null;
+	BufferedReader reader = null;
+	DBHelper db = null;
+	ProgressBar pb = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,67 @@ public class Splash extends Activity {
 	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.activity_splash);
+		
+		// TODO - implement database inits
+		db = new DBHelper(this);
+		pb = (ProgressBar) findViewById(R.id.splashProgress);
+		int progress = 0;
+		
+		if (db.tableIsEmpty(GameVars.TABLE_FIRST_NAMES)) {
+			initDB(R.raw.firstnames, GameVars.TABLE_FIRST_NAMES, GameVars.COLUMN_FIRST_NAME);
+			Log.d("DATABASE!!!", "First names has been populated!!");
+		} else {
+			Log.d("DATABASE!!!", "First names already populated!!");
+		}
+		// advance the loader progress bar
+		progress += 20;
+		pb.setProgress(progress);
+		
+		if (db.tableIsEmpty(GameVars.TABLE_LAST_NAMES)) {
+			initDB(R.raw.lastnames, GameVars.TABLE_LAST_NAMES, GameVars.COLUMN_LAST_NAME);
+			Log.d("DATABASE!!!", "Last names has been populated!!");
+		} else {
+			Log.d("DATABASE!!!", "Last names already populated!!");
+		}
+		// advance the loader progress bar
+		progress += 20;
+		pb.setProgress(progress);
+		
+		if (db.tableIsEmpty(GameVars.TABLE_CITY_NAMES)) {
+			initDB(R.raw.citynames, GameVars.TABLE_CITY_NAMES, GameVars.COLUMN_CITY_NAME);
+			Log.d("DATABASE!!!", "City names has been populated!!");
+		} else {
+			Log.d("DATABASE!!!", "City names already populated!!");
+		}
+		// advance the loader progress bar
+		progress += 20;
+		pb.setProgress(progress);
+		
+		if (db.tableIsEmpty(GameVars.TABLE_TEAM_NAMES)) {
+			initDB(R.raw.teamnames, GameVars.TABLE_TEAM_NAMES, GameVars.COLUMN_TEAM_NAME);
+			Log.d("DATABASE!!!", "Team names has been populated!!");
+		} else {
+			Log.d("DATABASE!!!", "Team names already populated!!");
+		}
+		// advance the loader progress bar
+		progress += 20;
+		pb.setProgress(progress);
+		
+		if (db.tableIsEmpty(GameVars.TABLE_NICKNAMES)) {
+			initDB(R.raw.nicknames, GameVars.TABLE_NICKNAMES, GameVars.COLUMN_NICKNAME);
+			Log.d("DATABASE!!!", "Team names has been populated!!");
+		} else {
+			Log.d("DATABASE!!!", "Team names already populated!!");
+		}
+		// advance the loader progress bar
+		progress += 20;
+		pb.setProgress(progress);
+		
+		db.close();
+		
+		// continue to the main menu screen
+		Intent intent = new Intent(this, NewGameOptions.class);
+		startActivity(intent);
 	}
 
 	@Override
@@ -23,6 +100,49 @@ public class Splash extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.splash, menu);
 		return true;
+	}
+	
+	private void initDB(int resource, String table, String column) {
+		try {
+			input = this.getResources().openRawResource(resource);
+			Log.d("TRACING!!", "STEP 1");
+			reader = new BufferedReader(new InputStreamReader(input));
+			Log.d("TRACING!!", "STEP 2");
+			Log.d("File Success!!!", "Reading file for " + table);
+			Log.d("TRACING!!", "STEP 3");
+			String fn;
+			Log.d("TRACING!!", "STEP 4");
+			while ((fn = reader.readLine()) != null) {
+				String[] names = fn.split(",");
+				Log.d("TRACING!!", "STEP 5 - Splitting File");
+				for (int i = 0; i < names.length; i++) {
+					// write first names to the database
+					db.addName(table, column, names[i]);
+				}
+				Log.d("TRACING!!", "STEP 6 - Finished File");
+			}
+		} catch(Exception e) {
+			Log.d("Failure!!", "Could not read file for " + table);
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+					Log.d("TRACING!!", "STEP 7 - INPUT CLOSE");
+					Log.d("Success!!", "Input stream connection closed.");
+				} catch (IOException e) {
+					Log.d("Failure!!", "Problem closing input stream connection.");
+				}
+			}
+			if (reader != null) {
+				try {
+					reader.close();
+					Log.d("TRACING!!", "STEP 8 - READER CLOSE");
+					Log.d("Success!!", "Buffered reader closed.");
+				} catch (IOException e) {
+					Log.d("Failure!!", "Problem closing buffered reader.");
+				}
+			}
+		}
 	}
 
 }
